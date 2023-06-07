81ba2ac02840a5cb42eedeac01d52cac1457205c
alfred <shaheenalfred48@gmail.com>
2023-06-07 16:08:41 -0700

    adding map and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import DownloadContract from "./components/DownloadContract";
 import Header from "./components/Header";
 import { ContactUs } from "./components/ContactUs";
+import Map from "./components/Map";
+import CopyRights from "./components/CopyRights";
 
 function App() {
 	
@@ -13,10 +15,12 @@ function App() {
 			<div className="App">
 				<Routes>
 					<Route path="/" element={<Header />} />
+					<Route path="/map" element={<Map />} /> 
 					<Route path="/download-contract" element={<DownloadContract />} />
 					<Route path="/contact-form" element={<ContactUs />} />
 				</Routes>
 			</div>
+				<CopyRights />
 		</>
 	);
 }
---
+++ src/components/CopyRights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CopyRights() {
+  return (
+    <div className='copyright'> <p>Copyrights © 2023 Mary Kopti Day Care, All Rights Reserved.</p></div>
+  )
+}
+
+export default CopyRights
